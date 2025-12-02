93a43980374412ed0538098f0b0c302967646bc5
Tzortzatos Ioannis <jtzortzatos2005@gmail.com>
2025-12-01 20:55:58 -0500

    Class Project, First work

---
+++ ClassProject/Assets/Scripts/TargetBehavior.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetBehavior : MonoBehaviour
+{
+    public int scoreAmount = 0;
+
+
+    // when collided with another gameObject
+    void OnCollisionEnter2D(Collision2D other)
+	{
+		Debug.Log("Collision Detected");
+		// only do stuff if collided with the Player
+		if (other.gameObject.tag == "Player")
+		{
+            // destroy self
+            Destroy(gameObject);
+            // if game manager exists, make adjustments based on target properties
+            if (GameManager.gm)
+            {
+                GameManager.gm.targetHit(scoreAmount);
+            }
+            
+		}
+	}
+
+}
